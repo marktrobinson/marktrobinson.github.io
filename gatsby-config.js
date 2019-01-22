4c2f446fd72e214d263c45593f768b3dd589423c
Mark Robinson <mark@rome2rio.com>
2019-01-23 06:02:04 +1100

    Adding GA Tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-52398068-4`,
       },
     },
     `gatsby-plugin-feed`,
